6eefb213e31ef953d2387f4ca735f78e12abbdb6
SuraJ7399 <95271793+SuraJ7399@users.noreply.github.com>
2021-12-09 16:01:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,65 +1,31 @@
 provider "aws" {
-   region     = "ap-northeast-1"
-   access_key = "AKIASIP36E5FHIZE7FCB"
-   secret_key = "6VHENbWpo6a8LKAnVwSiORpsO8Es9ut8IzeU59Dq"
-   
+
+region = "us-east-1"
+
 }
 
-resource "aws_instance" "ec2_example" {
+resource "aws_instance" "ubuntu" {
 
-    ami = "ami-0218d08a1f9dac831"  
-    instance_type = "t2.micro" 
-    key_name= "keyfile"
-    vpc_security_group_ids = [aws_security_group.main.id]
+ami = "ami-083654bd07b5da81d"
+instance_type = "t2.micro"
+key_name = "PEM"
 
-  provisioner "remote-exec" {
-    inline = [
-      "touch hello.txt",
-      "echo helloworld remote provisioner >> hello.txt",
-    ]
-  }
-  connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ubuntu"
-      private_key = file("/home/ec2-user/keyfile")
-      timeout     = "4m"
-   }
+}
+resource "aws_eip" "elastic_ip" {
+
+instance = aws_instance.ubuntu.id
+
 }
 
-resource "aws_security_group" "main" {
-  egress = [
-    {
-      cidr_blocks      = [ "0.0.0.0/0", ]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    }
-  ]
- ingress                = [
-   {
-     cidr_blocks      = [ "0.0.0.0/0", ]
-     description      = ""
-     from_port        = 22
-     ipv6_cidr_blocks = []
-     prefix_list_ids  = []
-     protocol         = "tcp"
-     security_groups  = []
-     self             = false
-     to_port          = 22
-  }
-  ]
-}
+resource "aws_security_group" "allow_ssh" {
 
+name = "allow_ssh"
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "keyfile"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2fy2KvAGqtXJsrsGAqAsr3X9EbWIYXLBGdWetyfRwYFxft5hi5dQkct/msP6H9W00OzpFVmzHFrwJNTckYPP/yoWXTP+DSYZcLY9nAWQlRh5BXQ/gLdv/Rpe7wkA3WhqmJJcLl
-9B4DoiJuZSF6TemttXSiyAFXsSnfpT4Zg0rjd+X7fBwhm+M4Pb6+yVXi05FbEgopFFCBzivOG9vnRPAyb2SNWD1mvPS93nL7h3HKVhza6eQ5bfOSVag2vI+l1Nnv+kFbGQT3hnUjrBpmZZbq4cnjMF2opqyMEJQAAJbbQLy/A3tcY
-NF38011leLvqEKilkBwX5P4QNVTrFGkT/r ec2-user@ip-172-31-32-20.ap-south-1.compute.internal"
+ingress {
+
+description = "ssh from VPC"
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
 }
