355991f9156c7b22e1a1427d8a92e738bd8e4916
SuraJ7399 <95271793+SuraJ7399@users.noreply.github.com>
2021-12-14 16:01:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,17 @@
 provider "aws" {
+    region     = "us-east-1"
+  }
 
-region = "ap-south-1"
+  resource "aws_instance" "ubuntu" {
+    ami           = "ami-083654bd07b5da81d"
+    instance_type = "t2.micro"
+    key_name = "PEM"
+  }
 
-}
+  resource "aws_eip" "elastic_ip" {
+    instance = aws_instance.ubuntu.id
+  }
 
-resource "aws_instance" "ubuntu" {
-
-ami = "ami-052cef05d01020f1d"
-instance_type = "t2.micro"
-key_name = "suraj"
-
-}
-resource "aws_eip" "elastic_ip" {
-instance = aws_instance.ubuntu.id
-}
-
-output "EIP_new" {
-value = aws_eip.elastic_ip.public_ip
-}
+  output "EIP" {
+    value = aws_eip.elastic_ip.public_ip
+  }
